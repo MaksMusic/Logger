47e1a6443b567ca293f753e7661747ce0772c5a9
MaksMusic <Maksmusic05@yandex.ru>
2023-10-04 12:51:52 +0300

    add work logger

---
+++ src/main/java/logi/AddLoggToFile.java
@@ -0,0 +1,52 @@
+package logi;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class AddLoggToFile {
+
+    private static final Logger ERROR_LOGGER = Logger.getLogger("errorLogger");
+    private static final Logger INFO_LOGGER = Logger.getLogger("infoLogger");
+
+    public static void main(String[] args) {
+        // Настройка логгеров
+        configureLoggers();
+
+        // Пример логирования ошибки
+        try {
+            throw new IOException("Пример ошибки");
+        } catch (IOException e) {
+            // Запись сообщения об ошибке в лог
+            ERROR_LOGGER.log(Level.SEVERE, "Ошибка ввода-вывода", e);
+        }
+
+        // Пример логирования информационного сообщения
+        INFO_LOGGER.info("Пример информационного сообщения");
+    }
+
+    private static void configureLoggers() {
+        try {
+            // Создание хэндлера для записи логов в файл
+            FileHandler errorFileHandler = new FileHandler("errors.log");
+            FileHandler infoFileHandler = new FileHandler("info.log");
+
+            // Форматирование записей логов
+            SimpleFormatter formatter = new SimpleFormatter();
+            errorFileHandler.setFormatter(formatter);
+            infoFileHandler.setFormatter(formatter);
+
+            // Установка уровней логирования
+            ERROR_LOGGER.setLevel(Level.SEVERE);
+            INFO_LOGGER.setLevel(Level.INFO);
+
+            // Добавляем хэндлеры в логгеры
+            ERROR_LOGGER.addHandler(errorFileHandler);
+            INFO_LOGGER.addHandler(infoFileHandler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/logi/AddingLoggDifferentToFile.java
@@ -0,0 +1,88 @@
+package logi;
+import java.io.IOException;
+import java.util.logging.*;
+
+public class AddingLoggDifferentToFile {
+    private static final Logger ERROR_LOGGER = ErrorLoggerInitializer.getLogger();
+    private static final Logger INFO_LOGGER = InfoLoggerInitializer.getLogger();
+
+    public static void main(String[] args) {
+        ErrorLoggerInitializer.initializeLogger();
+        InfoLoggerInitializer.initializeLogger();
+
+        ERROR_LOGGER.severe("Пример сообщения об ошибке");
+        INFO_LOGGER.info("Пример информационного сообщения");
+
+        WorkerClass worker = new WorkerClass();
+        worker.performWork();
+    }
+}
+
+class ErrorLoggerInitializer {
+    private static final Logger ERROR_LOGGER = Logger.getLogger("errorLogger");
+
+    public static void initializeLogger()  {
+        ERROR_LOGGER.setLevel(Level.SEVERE);
+        FileHandler fileHandlerErr = null;
+        try {
+            fileHandlerErr = new FileHandler("ErrorLogger.log");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        SimpleFormatter simpleFormatter = new SimpleFormatter();
+        //fileHandlerErr.setLevel(Level.SEVERE);
+
+        fileHandlerErr.setFormatter(simpleFormatter);
+        ERROR_LOGGER.addHandler(fileHandlerErr);
+
+    }
+
+    public static Logger getLogger() {
+        return ERROR_LOGGER;
+    }
+}
+
+
+class InfoLoggerInitializer {
+    private static final Logger INFO_LOGGER = Logger.getLogger("infoLogger");
+
+    public static void initializeLogger() {
+        INFO_LOGGER.setLevel(Level.INFO);
+
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(Level.INFO);
+
+        INFO_LOGGER.addHandler(consoleHandler);
+    }
+
+    public static Logger getLogger() {
+        return INFO_LOGGER;
+    }
+}
+
+class WorkerClass {
+    private static final Logger ERROR_LOGGER = ErrorLoggerInitializer.getLogger();
+    private static final Logger INFO_LOGGER = InfoLoggerInitializer.getLogger();
+
+    public void performWork() {
+        INFO_LOGGER.info("Выполняется работа");
+
+        HelperClass helper = new HelperClass();
+        helper.performAction();
+
+        // Генерирование ошибки для примера
+        try {
+            int result = 10 / 0;
+        } catch (Exception e) {
+            ERROR_LOGGER.log(Level.SEVERE, "Ошибка во время работы", e);
+        }
+    }
+}
+
+class HelperClass {
+    private static final Logger INFO_LOGGER = InfoLoggerInitializer.getLogger();
+
+    public void performAction() {
+        INFO_LOGGER.info("Выполняется действие performAction");
+    }
+}
